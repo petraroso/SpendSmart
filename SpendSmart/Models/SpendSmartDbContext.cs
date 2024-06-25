16257efde7024d1627153cd157a45999cf9656f5
petraroso <proso00@fesb.hr>
2024-06-25 15:34:44 +0200

    Connect to Entity Fr in-memory db

---
+++ SpendSmart/Models/SpendSmartDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        //constructor:
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) :base(options)
+        {
+                
+        }
+    }
+}
